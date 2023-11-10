f8331020d1701a87299ba1033a14601cc28a8a1c
agent-storm <gujarathisaisrinith@gmail.com>
2023-11-10 22:27:50 +0530

    update

--- public/profile.js
+++ public/profile.js
@@ -5,13 +5,9 @@ const projectsContainer = document.querySelector(".projects-container-div")
 window.addEventListener('scroll',() => {
     const scrollPx = window.scrollY;
     console.log(scrollPx);
-    // if(scrollPx>150){
-    //     window.scrollTo(730);
-    //     projectsContainer.scrollIntoView(true);
-    // }
-    if(scrollPx<730){
+    if(scrollPx<430){
         document.body.style.backgroundColor = "#FFFBFE";
-    } else if(scrollPx>730 && scrollPx<1300) {
-        document.body.style.backgroundColor = "#1A1B25";
+    } else if(scrollPx>930 && scrollPx<1300) {
+        document.body.style.backgroundColor = "#373F51"; //"#1A1B25"
     }
 });
