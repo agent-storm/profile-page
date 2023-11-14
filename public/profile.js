121994d3259833f88ffc6a72e81440d7c38f403c
agent-storm <gujarathisaisrinith@gmail.com>
2023-11-14 20:10:59 +0530

    scroll btns added

--- public/profile.js
+++ public/profile.js
@@ -4,10 +4,30 @@ function LoginPageSwitcher(){
 const projectsContainer = document.querySelector(".projects-container-div")
 window.addEventListener('scroll',() => {
     const scrollPx = window.scrollY;
-    console.log(scrollPx);
+    // console.log(scrollPx);
     if(scrollPx<430){
         document.body.style.backgroundColor = "#FFFBFE";
     } else if(scrollPx>830 && scrollPx<1300) {
         document.body.style.backgroundColor = "#373F51"; //"#1A1B25"
     }
 });
+function scrollToProjects(){
+    const element = document.getElementById("project-text");
+    element.scrollIntoView();
+}
+let curElementIndx = 0;
+let ProjectsList = Array("stormcode-app","auto-vid-editor")
+function ProjectScroll(direction) {
+    console.log(ProjectsList[curElementIndx]);
+    if(direction == "up") {
+        if(curElementIndx == 0) return;
+        curElementIndx-=1;
+    } 
+    else if (direction == "down") {
+        if(curElementIndx == 1) return;
+        curElementIndx+=1;
+    }
+    let projectDiv = document.getElementById(ProjectsList[curElementIndx]);
+    let scrollVal = projectDiv.offsetTop;
+    document.getElementById("projects-scrolling-div").scrollTop = scrollVal;
+}
